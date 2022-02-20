515839704080dee06092c8a9bfb9a5710523cfd4
antonymburia <antonymburia10@gmail.com>
2022-02-20 03:57:10 +0300

    create a functions to save and display users when we run

--- run.py
+++ run.py
@@ -1,11 +1,22 @@
 from details.user import User
 from details.credentials import Credentials
 
-def create_user(user_name, password):
+def create_new_user(user_name, password):
     '''
     this function creates a new user instance
     '''
     new_user = User(user_name, password)
 
     return new_user
+
+def save_new_user(user):
+    user.save_user
+
+def show_user(user):
+    '''
+    this function displays a user
+    '''
+    user.show_user()
+
+
 
